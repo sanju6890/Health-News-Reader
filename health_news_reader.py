bd79c490ada04bd75397563d9561de7bd2c50d11
SANJAY KUMAR <65277126+sanju6890@users.noreply.github.com>
2020-06-12 13:56:18 +0530

    Add files via upload

---
+++ health_news_reader.py
@@ -0,0 +1,31 @@
+# Read newspaper with python
+# using two important and popular python pacakages requests and json
+import requests
+import json
+# function for speech
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.speak(str)
+# main programme
+if __name__ == "__main__":
+    print("\n********** Here is Today's Health News ***********")
+    speak("Here is Today's health news")
+    url="http://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=3e33ba1948eb4504b47af11904512730"
+    news=requests.get(url).text # creating url request
+    news_dict=json.loads(news) # creating python object
+    contents=news_dict["articles"]
+    i=1
+    print('\n--------------------------------------------------------------------------------------------')
+    for content in contents: # iterating the news
+        speak(str(i))
+        print('(',i,')',content['title'],'\n')
+        speak(content['title'])
+        print('->',content['description'])
+        if(content['description']):
+            speak(content['description'])
+        i=i+1
+        print('\n----------------------------------------------------------------------------------------------')
+    speak("thank you for listening..")
+    print('\n********** Thank you for listening..... ***********')
+
